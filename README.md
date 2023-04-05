edc2e0daea55ea5d8bac731ba39e1667acb8894f
Yosiyas7 <josiahdebessay@gmail.com>
2023-04-05 11:36:22 +1000

    v3

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Coming soon
+# Tic Tac Toe
--- game.js
+++ game.js
@@ -1,24 +1,60 @@
 //declaring variables for players and board
-const player1 = 'x';
+const player1 = 'X';
 
-const player2 = 'y';
+let restartButton = $('#restartButton');
+
+const player2 = 'Y';
 
 //select current player
 let currentPlayer = player1;
 
 //give the boxes empty arrays
-const board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]];
+// this is to call the first nested array board[0][0]
+const board = [["_", "_", "_"], 
+        ["_", "_", "_"],
+        ["_", "_", "_"]];
 
-let playerText = $('#playerText');
-let restartButton = $('restartButton');
+const box = {
+    "0": "_",
+    "1": "_",
+    "2": "_",
+    "3": "_",
+    "4": "_",
+    "5": "_",
+    "6": "_",
+    "7": "_",
+    "8": "_",
+};
 
-const boxes = {
-    $('box').click(function () {
-        const boxClass = $(this).attr('id');
-        const row = parseInt((boxClass[6])-1);
-        const col = parseInt((boxClass[8])-1);
-    })
+
+//add event listener for all boxes and listen for a click
+$('.box').click() => {
+    const id = e.target.id;
+    const row = Math.floor(id/3);
+    const col = id % 3;
+
+    //check is the box is not already filled
+    if (board[row][col] === '_') {
+
+    // fill the box with the current player1 symbol
+        board[row][col] = currentPLayer;
+
+    }
 }
+
+function boxClicked(e) {
+    const id = e.target.id;
+  
+    // check if the space is not already filled
+    if (!spaces[id]) {
+      // fill the space with the current player's symbol
+      spaces[id] = currentPlayer;
+      e.target.innerText = currentPlayer;
+  
+      // switch to the other player's turn
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+  }
 
 //Add the game logic
 //If player 1 selects 3 squares in a row, player 1 wins
