34003374f29f077dbb1d1700b38bc04a47cdd959
Yosiyas7 <josiahdebessay@gmail.com>
2023-04-05 12:56:12 +1000

    v4

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Tic Tac Toe
+
+## Game Instructions
+
+### Tic Tac Toe is a *two-player* game, where each *player* takes turns to place their symbol (either **X** or **O**) on the board. The first player to get three of their symbols in a row (either horizontally, vertically, or diagonally) wins the game. If all the cells on the board are filled and no player has won, the game is a draw.
+
+[Link to the Game](https://yosiyas7.github.io/solid-rotary-phone/)
+
+
+
--- game.js
+++ game.js
@@ -1,61 +1,60 @@
 //declaring variables for players and board
-const player1 = 'X';
-
-let restartButton = $('#restartButton');
-
-const player2 = 'Y';
-
+$(function(){
+    const player1 = 'X';
+    const player2 = 'O';
 //select current player
 let currentPlayer = player1;
 
 //give the boxes empty arrays
 // this is to call the first nested array board[0][0]
-const board = [["_", "_", "_"], 
+const board = [
+        ["_", "_", "_"], 
         ["_", "_", "_"],
-        ["_", "_", "_"]];
+        ["_", "_", "_"]
+    ];
+// const box = {
+//     "0": "_",
+//     "1": "_",
+//     "2": "_",
+//     "3": "_",
+//     "4": "_",
+//     "5": "_",
+//     "6": "_",
+//     "7": "_",
+//     "8": "_",
+// };
+//add event listener for all boxes and listen for a click. Execute the
+const clickedBox = function() {
+    $('.box').on('click', function() {
+        const id = this.id;
+        const row = Math.floor(id/3);
+        const col = id % 3;
+    
+        //check is the box is not already filled
+        if (board[row][col] === '_') {
+    
+        // fill the box with the current player's symbol
+            board[row][col ] = currentPlayer;
+            $(this).text(currentPlayer);
 
-const box = {
-    "0": "_",
-    "1": "_",
-    "2": "_",
-    "3": "_",
-    "4": "_",
-    "5": "_",
-    "6": "_",
-    "7": "_",
-    "8": "_",
-};
-
-
-//add event listener for all boxes and listen for a click
-$('.box').click() => {
-    const id = e.target.id;
-    const row = Math.floor(id/3);
-    const col = id % 3;
-
-    //check is the box is not already filled
-    if (board[row][col] === '_') {
-
-    // fill the box with the current player1 symbol
-        board[row][col] = currentPLayer;
-
-    }
+            //switch to the other player's turn
+            if (currentPlayer === player1) {
+                currentPlayer = player2;
+              } else {
+                currentPlayer = player1;
+              }
+        }
+    })
 }
 
-function boxClicked(e) {
-    const id = e.target.id;
-  
-    // check if the space is not already filled
-    if (!spaces[id]) {
-      // fill the space with the current player's symbol
-      spaces[id] = currentPlayer;
-      e.target.innerText = currentPlayer;
-  
-      // switch to the other player's turn
-      currentPlayer = currentPlayer === player1 ? player2 : player1;
-    }
-  }
-
+//call the functioin to add click event listener to boxes
+clickedBox();
+    // Add click event listener to restart button
+    $('#restartButton').click(() => {
+        // Reload the page
+        location.reload();
+      });
+});
 //Add the game logic
 //If player 1 selects 3 squares in a row, player 1 wins
 //Check diagonally, up and across
